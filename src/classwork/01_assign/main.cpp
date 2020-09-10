319140120e4b2a781b35b40fe57a7e3c345d6d86
stellaMillerAcc <stella.miller@g.austincc.edu>
2020-09-10 11:19:15 -0500

    Homework 0 Submission - write a function that multiples a user supplied integer by 5

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,27 @@
 //Write the include statement for types.h here
-
+#include "types.h";
 //Write include for capturing input from keyboard and displaying output to screen
+#include<iostream>;
 
-
+using std::cin;
+using std::cout;
 
 int main() 
 {
+	int num;
+
+	cout<<"Enter a Number: \n";
+	cin>>num;
+
+	int result = multiply_numbers(num);
+	cout<<"Result: \n";
+	cout<<result;
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+
+	cout<<"\nResult 2: \n";;
+	cout<<result;
 
 	return 0;
 }
