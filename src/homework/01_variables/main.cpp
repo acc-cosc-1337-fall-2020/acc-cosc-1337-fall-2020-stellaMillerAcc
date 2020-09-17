65bdcb8f3ebbf73c29c176f427e0efb248e3d687
stellaMillerAcc <stella.miller@g.austincc.edu>
2020-09-16 21:52:31 -0500

    Homework 1 - Meal Receipt Generator

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,35 @@
 ﻿//write include statements
-
+#include<iostream>
+#include "variables.h"
 
 //write namespace using statement for cout
+using std::cin;
+using std::cout;
 
-
-/*
-Call multiply_numbers with 10 and 10 parameter values and display function result
-*/
 int main()
 {
-	
+	double mealAmount;
+	double tipRate;
+	double tipAmount;
+	double taxAmount;
+	double total;
+
+	cout<<"Enter Meal Amount:\n";
+	cin>>mealAmount;
+
+	cout<<"Enter Tip Rate:\n";
+	cin>>tipRate;
+
+	taxAmount = get_sales_tax_amount(mealAmount);
+	tipAmount = get_tip_amount(mealAmount, tipRate);
+	total = mealAmount + taxAmount + tipAmount;
+
+	cout<<"ORDER RECEIPT\n";
+	cout<<"Meal Amount: " << mealAmount << "\n";
+	cout<<"Sales Tax: " << taxAmount << "\n";
+	cout<<"Tip Amount: "<< tipAmount << "\n";
+	cout<<"---------------" << "\n";
+	cout<<"Total: " << total;
 
 	return 0;
 }
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -6,9 +6,16 @@ int add_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
-//write code for multiply_numbers function here
-//function returns product of num1 and num2
+//Homework 1
+const double TAX_RATE = .0675;
 
+double get_sales_tax_amount(double mealAmount)
+{
+	return mealAmount*TAX_RATE;
+}
 
-
+double get_tip_amount(double mealAmount, double tipRate)
+{
+	return mealAmount*tipRate;
+}
 
