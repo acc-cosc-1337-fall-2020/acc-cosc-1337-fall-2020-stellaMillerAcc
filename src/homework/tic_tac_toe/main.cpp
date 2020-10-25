0950a8fd1f2751325d25c9f2b97eae540455c4d4
stellaMillerAcc <stella.miller@g.austincc.edu>
2020-10-24 20:58:13 -0500

    Homework 5

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,32 @@
+#include"tic_tac_toe.h"
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() 
 {
+	string firstPlayer;
+	TicTacToe game;
+	int position;
+
+	cout<<"---Welcome to Tic Tac Toe!---\n";
+	cout<<"Enter first player (X or O): ";
+	cin>>firstPlayer;
+
+	game.start_game(firstPlayer);
+
+	while (!game.game_over()) {
+		game.display_board();
+		cout<<"Enter board positon: ";
+		cin>>position;
+		game.mark_board(position);
+	}
+
+	cout<<" \n";
+	cout<<"Game Over! Final Board: \n";
+	game.display_board();
 
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,60 @@
 //cpp
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include"tic_tac_toe.h"
+
+using std::string;
+using std::vector;
+using std::find;
+using std::fill;
+using std::cout;
+
+bool TicTacToe::game_over() {
+    return check_board_full();
+}
+
+void TicTacToe::start_game(string first_player) {
+    if (first_player != "X" && first_player != "O") {
+        cout<<"Player may only be 'X' or 'O'";
+        return;
+    }
+    player = first_player;
+}
+
+void TicTacToe::mark_board(int position) {
+    pegs[position-1] = player;
+    set_next_player();
+}
+
+string TicTacToe::get_player() const {
+    return player;
+}
+
+void TicTacToe::display_board() const {
+    cout<<"\n";
+    cout<<" ------------ \n";
+    cout<<"| "<<pegs[0]<<" | "<<pegs[1]<<" | "<<pegs[2]<<" |\n";
+    cout<<" ------------ \n";
+    cout<<"| "<<pegs[3]<<" | "<<pegs[4]<<" | "<<pegs[5]<<" |\n";
+    cout<<" ------------ \n";
+    cout<<"| "<<pegs[6]<<" | "<<pegs[7]<<" | "<<pegs[8]<<" |\n";
+    cout<<" ------------ \n";
+    cout<<"\n";
+}
+
+void TicTacToe::set_next_player() {
+    if (player == "X") {
+        player = "O";
+    }
+
+    else player = "X";
+}
+
+bool TicTacToe::check_board_full() {
+    return find(pegs.begin(), pegs.end(), " ") == pegs.end();
+}
+
+void TicTacToe::clear_board() {
+    fill(pegs.begin(), pegs.end(), " ");
+}
